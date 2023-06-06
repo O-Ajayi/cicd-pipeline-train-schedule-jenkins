9344b2386a044ffafed66d1bb1d4bb6c23788545
o-ajayi2k1 <o.ajayi2k1@gmail.com>
2023-06-06 13:59:57 -0500

    updated jenkins file for smm creation

--- build/deploy-ssm.sh
+++ build/deploy-ssm.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 echo "Running this from the bash script"
-pwd
+
 # CURRENT_DIR=$(pwd)
 # INPUT="${CURRENT_DIR}../ssm/dev/dev.csv"
-# OLDIFS=$IFS
-# IFS=','
-# [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-# while read -r key value
-# do
-# 	echo "Testing"
-# 	echo "Key : $key"
-# 	echo "Value : $value"
+INPUT="${CURRENT_DIR}/ssm/dev/dev.csv"
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read -r key value
+do
+	echo "Testing"
+	echo "Key : $key"
+	echo "Value : $value"
 
-# 	aws ssm put-parameter \
-# 	    --name "${key}" \
-# 	    --value "${value}" \
-# 	    --type "SecureString" \
-# 	    --overwrite 
-# 	    # --no-cli-pager
+	aws ssm put-parameter \
+	    --name "${key}" \
+	    --value "${value}" \
+	    --type "SecureString" \
+	    --overwrite 
+	    # --no-cli-pager
 
-# done < $INPUT
-# # IFS=$OLDIFS
+done < $INPUT
+# IFS=$OLDIFS
 
-# echo "End of ssm deployment"
+echo "End of ssm deployment"
