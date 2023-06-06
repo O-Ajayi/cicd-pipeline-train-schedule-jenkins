8bba4895064e1f51bbac88ebc4466524463a7438
o-ajayi2k1 <o.ajayi2k1@gmail.com>
2023-06-06 13:47:29 -0500

    updated jenkins file for smm creation

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
     stage ('Build') {
       steps {
         echo 'Running build automation'
-        // sh './gradlew build --no-daemon'
+        sh './build/deploy-ssm.sh'
         // archiveArtifacts artifacts: 'dist/trainSchedule.zip'
       }
     }
---
+++ build/deploy-ssm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+INPUT="${CURRENT_DIR}../ssm/dev/dev.csv"
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read -r key value
+do
+	echo "Testing"
+	echo "Key : $key"
+	echo "Value : $value"
+
+	aws ssm put-parameter \
+	    --name "${key}" \
+	    --value "${value}" \
+	    --type "SecureString" \
+	    --overwrite 
+	    # --no-cli-pager
+
+done < $INPUT
+# # IFS=$OLDIFS
+
+# echo "End of ssm deployment"
